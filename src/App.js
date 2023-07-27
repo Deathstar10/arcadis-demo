aa0a84ce402e1ec76bc0815a5527f90d3e798ca9
Aravind Samala <aravind.samala96@gmail.com>
2023-07-27 12:59:38 +0530

    search for movies on user input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
+const API_KEY = "982ff953";
 function App() {
+  const [movie, setMovie] = useState("");
+  const [movieDetails, setMovieDetails] = useState({});
+  const [showMovie, setShowMovies] = useState(false);
+  const [paginationNumbers, setPaginationNumbers] = useState([]);
+
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+      const data = await fetchMovieData();
+      setMovieDetails(data);
+      setShowMovies(true);
+
+      const movies = Math.ceil(Number(data["totalResults"]));
+
+      let paginatedMovies = [];
+      for (let index = 1; index <= movies; index++) {
+        paginatedMovies.push(index);
+      }
+      setPaginationNumbers(paginatedMovies);
+    } catch (err) {
+      console.log("Error");
+    }
+  }
+
+  async function fetchMovieData(page = 1) {
+    const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${movie}&p=${page}`;
+    const data = await fetch(url).then((res) => res.json());
+
+    return data;
+  }
+
+  function handleClick(num) {
+    const data = fetchMovieData(num);
+    setMovieDetails(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Movies</h1>
+
+      {/* form */}
+      <form onSubmit={handleSubmit}>
+        <label>Movie name:</label>
+        <input
+          value={movie}
+          onChange={(e) => setMovie(e.target.value)}
+          placeholder="Enter a movie name"
+        />
+      </form>
+      {showMovie && (
+        <div>
+          {movieDetails &&
+            movieDetails["Search"] &&
+            movieDetails["Search"].map((data) => (
+              <div className="movie-card">
+                <p>{data.Title}</p>
+                <p>{data.Year}</p>
+              </div>
+            ))}
+        </div>
+      )}
+
+      {paginationNumbers &&
+        paginationNumbers.length > 0 &&
+        paginationNumbers.map((num) => {
+          <button onClick={handleClick(num)} key={num}>
+            {num}
+          </button>;
+        })}
+    </>
   );
 }
 
